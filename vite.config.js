05361d82bd724b05550a2efa7733f7305acd8c13
bashir abubakar <bashir.abubakar2009@gmail.com>
2025-12-13 18:32:18 +0000

    Fix GitHub Pages script path

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  
-  // Correct base path for a GitHub user site:
   base: "/bashir-abubakar.github.io/",
-
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
 });
